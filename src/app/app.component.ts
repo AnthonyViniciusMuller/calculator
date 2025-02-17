907d2864debd2d1707d58ded7b368dd63f44f0e3
Anthony Muller <anthonyvmuller@outlook.com>
2025-02-16 21:42:48 -0300

    feat: connect all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HistoryComponent } from "./components/history/history.component";
+import { PreviewComponent } from "./components/preview/preview.component";
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [KeyboardComponent, HistoryComponent, PreviewComponent, ToolbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'calculator';
-}
+export class AppComponent { }
